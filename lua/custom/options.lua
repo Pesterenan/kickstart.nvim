c47b8e91e99af275efd0aaa9b0ed5813b29b4222
Pesterenan <pesterenan@gmail.com>
2023-07-13 00:38:29 -0300

    -- Added options for smartindent and tabspacing, set very magic option for regex substitution

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,11 +1,15 @@
 -- Custom options - Pesterenan
 
 -- Set number lines and relative number lines:
-vim.opt.number = true 
+vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.magic = true
 
 -- 4 indent spaces, incremental search highlighting, no line wrap:
-vim.opt.tabstop = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 vim.opt.incsearch = true
 vim.opt.wrap = false
 
