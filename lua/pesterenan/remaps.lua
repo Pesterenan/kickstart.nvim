34c7c305905a6cafe91af05a7f32c576be051fe8
Pesterenan <pesterenan@gmail.com>
2023-07-16 17:48:41 -0300

    -- Changed to transparent background, added remaps for git

--- lua/pesterenan/remaps.lua
+++ lua/pesterenan/remaps.lua
@@ -20,3 +20,8 @@ vim.keymap.set('n','<C-h>', '<cmd>wincmd h<CR>')
 vim.keymap.set('n','<C-j>', '<cmd>wincmd j<CR>')
 vim.keymap.set('n','<C-k>', '<cmd>wincmd k<CR>')
 vim.keymap.set('n','<C-l>', '<cmd>wincmd l<CR>')
+
+-- Git related keymaps:
+vim.keymap.set('n','<leader>gs', "<cmd>Git<CR><cmd>Git status<CR>", { desc = "Show [G]it [S]tatus"})
+vim.keymap.set('n','<leader>gp', "<cmd>Git push<CR>", { desc = "[G]it [P]ush"})
+
