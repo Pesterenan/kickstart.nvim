5258e8b1fb8db5d8ede7ce0f58849cc717919058
Pesterenan <pesterenan@gmail.com>
2023-07-19 14:47:32 -0300

    -- Set autoindent to true, simplified Git Status remap

--- lua/pesterenan/options.lua
+++ lua/pesterenan/options.lua
@@ -9,6 +9,7 @@ vim.opt.magic = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.incsearch = true
 vim.opt.wrap = false
