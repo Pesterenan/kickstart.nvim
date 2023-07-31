9f5db234f9e7d59d1ac3b07edd371cc75c43b2bc
Pesterenan <pesterenan@gmail.com>
2023-07-31 09:34:53 -0300

    -- Added gruvbox, molokai colorthemes

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,10 +1,14 @@
 -- Sets colorscheme to rose-pine
 return {
-	'rose-pine/neovim',
-	config = function()
-		vim.cmd('colorscheme rose-pine')
-		-- Set active buffer background color to be a little lighter
-		vim.api.nvim_set_hl(0,"Normal", { bg = '#272033'})
-		vim.api.nvim_set_hl(0,"NormalFloat", { bg = '#272033'})
-	end
+	{
+		'rose-pine/neovim',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+			-- Set active buffer background color to be a little lighter
+			vim.api.nvim_set_hl(0, "Normal", { bg = '#272033' })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = '#272033' })
+		end
+	},
+	{ 'morhetz/gruvbox' },
+	{ 'tomasr/molokai' },
 }
