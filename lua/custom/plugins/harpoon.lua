f7d56472913a4e9ee8aad8608ee45d404c8297bd
Pesterenan <pesterenan@gmail.com>
2024-01-21 12:53:19 -0300

    fix(keymaps): configure harpoon keymaps, remove window nav keymaps

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -13,13 +13,13 @@ return {
 		vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
 			{ desc = 'Toggle Harpoon quick menu' })
 
-		vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end,
+		vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end,
 			{ desc = 'Select first file on harpoon' })
-		vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end,
+		vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end,
 			{ desc = 'Select second file on harpoon' })
-		vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end,
+		vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end,
 			{ desc = 'Select third file on harpoon' })
-		vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end,
+		vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end,
 			{ desc = 'Select fourth file on harpoon' })
 
 		-- Toggle previous & next buffers stored within Harpoon list
