1d913930ef8207b93129b050db63efbe7f80f90d
Pesterenan <pesterenan@gmail.com>
2024-07-31 23:29:29 -0300

    [Java]: Including Java LSP and debug config

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -21,11 +21,12 @@ return {
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
     {
-      "mxsdev/nvim-dap-vscode-js",
-      dependencies = { "mfussenegger/nvim-dap" }
+      'mxsdev/nvim-dap-vscode-js',
+      dependencies = { 'mfussenegger/nvim-dap' },
     },
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    'mfussenegger/nvim-jdtls',
   },
   keys = {
     -- Basic debugging keymaps, feel free to change to your liking!
@@ -147,5 +148,6 @@ return {
         detached = vim.fn.has 'win32' == 0,
       },
     }
+    require('jdtls').setup_dap { hotcodereplace = 'auto' }
   end,
 }
