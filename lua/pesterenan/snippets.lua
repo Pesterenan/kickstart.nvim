4072dfec1d0ffd0c16ac732bf1886f2568c93b77
Pesterenan <pesterenan@gmail.com>
2024-07-31 23:34:39 -0300

    fix: add fugitive back to my plugins

--- lua/pesterenan/snippets.lua
+++ lua/pesterenan/snippets.lua
@@ -1,4 +1,4 @@
-local ls = require("luasnip")
+local ls = require 'luasnip'
 
 local s = ls.snippet
 local sn = ls.snippet_node
@@ -8,38 +8,38 @@ local f = ls.function_node
 local c = ls.choice_node
 local d = ls.dynamic_node
 local r = ls.restore_node
-local l = require("luasnip.extras").lambda
-local rep = require("luasnip.extras").rep
-local p = require("luasnip.extras").partial
-local m = require("luasnip.extras").match
-local n = require("luasnip.extras").nonempty
-local dl = require("luasnip.extras").dynamic_lambda
-local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local types = require("luasnip.util.types")
-local conds = require("luasnip.extras.conditions")
-local conds_expand = require("luasnip.extras.conditions.expand")
+local l = require('luasnip.extras').lambda
+local rep = require('luasnip.extras').rep
+local p = require('luasnip.extras').partial
+local m = require('luasnip.extras').match
+local n = require('luasnip.extras').nonempty
+local dl = require('luasnip.extras').dynamic_lambda
+local fmt = require('luasnip.extras.fmt').fmt
+local fmta = require('luasnip.extras.fmt').fmta
+local types = require 'luasnip.util.types'
+local conds = require 'luasnip.extras.conditions'
+local conds_expand = require 'luasnip.extras.conditions.expand'
 
 -- If you're reading this file for the first time, best skip to around line 190
 -- where the actual snippet-definitions start.
 
 -- Every unspecified option will be set to the default.
-ls.setup({
+ls.setup {
   keep_roots = true,
   link_roots = true,
   link_children = true,
 
   -- Update more often, :h events for more info.
-  update_events = "TextChanged,TextChangedI",
+  update_events = 'TextChanged,TextChangedI',
   -- Snippets aren't automatically removed if their text is deleted.
   -- `delete_check_events` determines on which events (:h events) a check for
   -- deleted snippets is performed.
   -- This can be especially useful when `history` is enabled.
-  delete_check_events = "TextChanged",
+  delete_check_events = 'TextChanged',
   ext_opts = {
     [types.choiceNode] = {
       active = {
-        virt_text = { { "choiceNode", "Comment" } },
+        virt_text = { { 'choiceNode', 'Comment' } },
       },
     },
   },
@@ -50,7 +50,7 @@ ls.setup({
   enable_autosnippets = true,
   -- mapping for cutting selected text so it's usable as SELECT_DEDENT,
   -- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
-  store_selection_keys = "<Tab>",
+  store_selection_keys = '<Tab>',
   -- luasnip uses this function to get the currently active filetype. This
   -- is the (rather uninteresting) default, but it's possible to use
   -- eg. treesitter for getting the current filetype by setting ft_func to
@@ -58,9 +58,9 @@ ls.setup({
   -- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
   -- the current filetype in eg. a markdown-code block or `vim.cmd()`.
   ft_func = function()
-    return vim.split(vim.bo.filetype, ".", true)
+    return vim.split(vim.bo.filetype, '.', true)
   end,
-})
+}
 
 -- args is a table, where 1 is the text in Placeholder 1, 2 the text in
 -- placeholder 2,...
@@ -75,19 +75,19 @@ rec_ls = function()
     nil,
     c(1, {
       -- Order is important, sn(...) first would cause infinite loop of expansion.
-      t(""),
-      sn(nil, { t({ "", "\t\\item " }), i(1), d(2, rec_ls, {}) }),
+      t '',
+      sn(nil, { t { '', '\t\\item ' }, i(1), d(2, rec_ls, {}) }),
     })
   )
 end
 
 -- Add my custom snippets
-ls.add_snippets("javascript", {
-  s("log", {
-    t("console.log(\""),
+ls.add_snippets('javascript', {
+  s('log', {
+    t 'console.log("',
     f(copy, 1),
-    t(": \","),
-    i(1, "variable"),
-    t(");"),
+    t ': ",',
+    i(1, 'variable'),
+    t ');',
   }),
 })
