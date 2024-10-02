fc1cc445d1a20b40b12ee38ae4b236c81d49a4c8
Pesterenan <pesterenan@gmail.com>
2024-10-02 08:56:05 -0300

    fix(Plugins): harpoon add method, trouble keymaps

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -9,27 +9,27 @@ return {
     -- REQUIRED
 
     vim.keymap.set('n', '<leader>a', function()
-      harpoon:list():append()
-    end, { desc = '[A]ppend current file to Harpoon' })
+      harpoon:list():add()
+    end, { desc = '[A]dd current file to Harpoon' })
     vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = 'Toggle Harpoon quick menu' })
 
-    vim.keymap.set('n', '<C-h>', function()
+    vim.keymap.set('n', '<A-h>', function()
       harpoon:list():select(1)
-    end, { desc = 'Select first file on harpoon' })
-    vim.keymap.set('n', '<C-j>', function()
+    end, { desc = 'Select first file on Harpoon' })
+    vim.keymap.set('n', '<A-j>', function()
       harpoon:list():select(2)
-    end, { desc = 'Select second file on harpoon' })
-    vim.keymap.set('n', '<C-k>', function()
+    end, { desc = 'Select second file on Harpoon' })
+    vim.keymap.set('n', '<A-k>', function()
       harpoon:list():select(3)
-    end, { desc = 'Select third file on harpoon' })
-    vim.keymap.set('n', '<C-l>', function()
+    end, { desc = 'Select third file on Harpoon' })
+    vim.keymap.set('n', '<A-l>', function()
       harpoon:list():select(4)
-    end, { desc = 'Select fourth file on harpoon' })
+    end, { desc = 'Select fourth file on Harpoon' })
 
     -- Toggle previous & next buffers stored within Harpoon list
-    -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-    -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+    vim.keymap.set("n", "<A-S-P>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<A-S-N>", function() harpoon:list():next() end)
   end,
 }
