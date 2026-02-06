ae3d1363fca180505f2f1851433224b1f403cdc3
Pesterenan <pesterenan@gmail.com>
2026-02-06 08:09:12 -0300

    feat: creating my configurations

---
+++ lua/custom/pesterenan/options.lua
@@ -0,0 +1,23 @@
+-- Custom options - Pesterenan
+
+-- Set relative number lines and magic search:
+vim.opt.relativenumber = true
+vim.opt.magic = true
+
+-- 2 indent spaces, incremental search highlighting, no line wrap:
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.wrap = false
+
+vim.opt.colorcolumn = '0'
+vim.opt.updatetime = 100
+vim.opt.termguicolors = true
+vim.o.winborder = 'rounded'
+
+vim.o.laststatus = 3
---
+++ lua/custom/pesterenan/remaps.lua
@@ -0,0 +1,26 @@
+-- My remaps - Pesterenan:
+
+-- On normal mode, when searching for the next word, center the term on the screen:
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Also center when moving half a screen:
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- Move selected lines up and down, even inside functions: (from ThePrimeagen)
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Open netrw to explore files: (from ThePrimeagen)
+vim.keymap.set('n', '<leader>pv', '<cmd>Ex<CR>', { desc = 'Open NetRW' })
+
+-- Navigate out of Terminal buffers
+vim.keymap.set('t', '<C-H>', '<C-\><C-N><cmd>wincmd h<CR>')
+vim.keymap.set('t', '<C-J>', '<C-\><C-N><cmd>wincmd j<CR>')
+vim.keymap.set('t', '<C-K>', '<C-\><C-N><cmd>wincmd k<CR>')
+vim.keymap.set('t', '<C-L>', '<C-\><C-N><cmd>wincmd l<CR>')
+
+-- Stay indenting in visual mode
+vim.keymap.set('v', '<', '<gv', { desc = 'Less indentation' })
+vim.keymap.set('v', '>', '>gv', { desc = 'More indentation' })
