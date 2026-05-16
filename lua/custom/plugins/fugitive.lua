7eceb6bbdbb0808869bf15ce64625da09d5dda16
Pesterenan <pesterenan@gmail.com>
2026-05-16 00:05:45 -0300

    feat: migrate fugitive to vim.pack

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -1,20 +1,16 @@
-return {
-  -- Git related plugins
-  {
-    'tpope/vim-fugitive',
-    dependencies = {
-      'tpope/vim-rhubarb',
-    },
-    config = function()
-      -- Git related keymaps:
-      vim.keymap.set('n', '<leader>gs', '<cmd>Git<CR>', { desc = 'Show [G]it [S]tatus' })
-      vim.keymap.set('n', '<leader>gp', '<cmd>Git push<CR>', { desc = '[G]it [P]ush' })
-      vim.keymap.set('n', '<leader>gl', '<cmd>Git log --oneline<CR>', { desc = '[G]it [L]og one line' })
-      vim.keymap.set('n', '<leader>gf', '<cmd>Git fetch -p<CR>', { desc = '[G]it [F]etch and prune' })
-
-      -- While on difftool get changes from either left or right side: (from ThePrimeagen)
-      vim.keymap.set('n', '<leader>gH', '<cmd>diffget //2<CR>', { desc = 'Get left side changes on difftool' })
-      vim.keymap.set('n', '<leader>gL', '<cmd>diffget //3<CR>', { desc = 'Get right side changes on difftool' })
-    end,
-  },
+local plugins = {
+  'https://github.com/tpope/vim-rhubarb',
+  'https://github.com/tpope/vim-fugitive',
 }
+
+vim.pack.add(plugins)
+
+-- Git related keymaps:
+vim.keymap.set('n', '<leader>gs', '<cmd>Git<CR>', { desc = 'Show [G]it [S]tatus' })
+vim.keymap.set('n', '<leader>gp', '<cmd>Git push<CR>', { desc = '[G]it [P]ush' })
+vim.keymap.set('n', '<leader>gl', '<cmd>Git log --oneline<CR>', { desc = '[G]it [L]og one line' })
+vim.keymap.set('n', '<leader>gf', '<cmd>Git fetch -p<CR>', { desc = '[G]it [F]etch and prune' })
+
+-- While on difftool get changes from either left or right side: (from ThePrimeagen)
+vim.keymap.set('n', '<leader>gH', '<cmd>diffget //2<CR>', { desc = 'Get left side changes on difftool' })
+vim.keymap.set('n', '<leader>gL', '<cmd>diffget //3<CR>', { desc = 'Get right side changes on difftool' })
