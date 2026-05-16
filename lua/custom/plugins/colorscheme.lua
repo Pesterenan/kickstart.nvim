3edeb295ac23feac3a91a5dea63e68638efe3f82
Pesterenan <pesterenan@gmail.com>
2026-05-16 10:48:31 -0300

    feat: migrate colorschemes to vim.pack

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,42 +1,25 @@
--- Colorscheme setup:
-return {
-  {
-    -- Plugin to make window's background transparent
-    'xiyaowong/nvim-transparent',
-  },
-  -- Colorschemes:
-  {
-    'tanvirtin/monokai.nvim',
-  },
-  {
-    'rose-pine/neovim',
-    config = function()
-      require('rose-pine').setup {
-        dim_nc_background = true,
-        disable_italics = true,
-      }
-    end,
-  },
-  {
-    'ellisonleao/gruvbox.nvim',
-    config = function()
-      require('gruvbox').setup {
-        dim_inactive = true,
-      }
-    end,
-  },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    config = function()
-      require('catppuccin').setup {
-        dim_inactive = {
-          enabled = true,
-          shade = 'dark',
-          percentage = 0.20,
-        },
-      }
-    end,
+local plugins = {
+  'https://github.com/xiyaowong/nvim-transparent',
+  'https://github.com/tanvirtin/monokai.nvim',
+  'https://github.com/rose-pine/neovim',
+  'https://github.com/ellisonleao/gruvbox.nvim',
+  'https://github.com/catppuccin/nvim',
+}
+
+vim.pack.add(plugins)
+
+-- Colorschemes setup:
+require('rose-pine').setup {
+  dim_nc_background = true,
+  disable_italics = true,
+}
+require('gruvbox').setup {
+  dim_inactive = true,
+}
+require('catppuccin').setup {
+  dim_inactive = {
+    enabled = true,
+    shade = 'dark',
+    percentage = 0.20,
   },
 }
