fb1ef880cf3b7df91aec7f74460c90fd09be9c50
Pesterenan <pesterenan@gmail.com>
2026-05-16 00:06:13 -0300

    feat: migrate blink, lualine to vim.pack

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -1,118 +1,43 @@
-return {
-  { -- Autocompletion
-    'saghen/blink.cmp',
-    lazy = false, -- Load early for LSP
-    version = '1.*',
-    dependencies = {
-      -- Snippet Engine
-      {
-        'L3MON4D3/LuaSnip',
-        version = '2.*',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then return end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-        opts = {},
+require('blink.cmp').setup {
+  keymap = {
+    preset = 'default',
+    ['<c-m>'] = { 'select_and_accept', 'fallback' },
+    ['<c-space>'] = { 'show', 'fallback' },
+  },
+
+  completion = {
+    list = {
+      selection = {
+        preselect = true,
+        auto_insert = true,
       },
-      'folke/lazydev.nvim',
     },
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      keymap = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- All presets have the following mappings:
-        -- C-space: Open menu or open docs if already open
-        -- C-n/C-p or Up/Down: Select next/previous item
-        -- C-e: Hide menu
-        -- C-k: Toggle signature help (if signature.enabled = true)
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
-        ['<c-m>'] = { 'select_and_accept', 'fallback' },
-        ['<c-space>'] = { 'show', 'fallback' },
-      },
-
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono',
-      },
-
-      completion = {
-        list = {
-          selection = {
-            preselect = true,
-            auto_insert = true,
-          },
-        },
-        keyword = {
-          range = 'prefix',
-        },
-        -- Show documentation window automatically to see full import paths
-        documentation = { auto_show = true, auto_show_delay_ms = 200 },
-        ghost_text = { enabled = true },
-        menu = {
-          auto_show = true,
-          auto_show_delay_ms = 100,
-          draw = {
-            columns = {
-              { 'kind_icon' },
-              { 'label', 'label_description', gap = 1 },
-              { 'source_name' },
-            },
-          },
-        },
-        accept = {
-          create_undo_point = true,
-        },
-        trigger = {
-          show_in_snippet = true,
-          show_on_trigger_character = true,
-          show_on_accept_on_trigger_character = true,
-          show_on_blocked_trigger_characters = { '\n', '\t' },
-          show_on_keyword = true, -- Mostra ao digitar qualquer caractere
+    keyword = {
+      range = 'prefix',
+    },
+    -- Show documentation window automatically to see full import paths
+    documentation = { auto_show = true, auto_show_delay_ms = 500 },
+    ghost_text = { enabled = true },
+    menu = {
+      auto_show = true,
+      auto_show_delay_ms = 100,
+      draw = {
+        columns = {
+          { 'kind_icon' },
+          { 'label', 'label_description', gap = 1 },
+          { 'source_name' },
         },
       },
-
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
-        providers = {
-          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-        },
-      },
-
-      snippets = { preset = 'luasnip' },
-
-      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-      -- which automatically downloads a prebuilt binary when enabled.
-      --
-      -- By default, we use the Lua implementation instead, but you may enable
-      -- the rust implementation via `'prefer_rust_with_warning'`
-      --
-      -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
-
-      -- Shows a signature help window while you type arguments for a function
-      signature = { enabled = true },
+    },
+    accept = {
+      create_undo_point = true,
+    },
+    trigger = {
+      show_in_snippet = true,
+      show_on_trigger_character = true,
+      show_on_accept_on_trigger_character = true,
+      show_on_blocked_trigger_characters = { '\n', '\t' },
+      show_on_keyword = true,
     },
   },
 }
